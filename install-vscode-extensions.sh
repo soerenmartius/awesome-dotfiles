0874a3e49550c86aa61644fa3ea33ec2f311374d
Soren Martius <soeren.martius@gmail.com>
2020-06-03 12:23:51 +0200

    add new vscode extensions

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -9,6 +9,7 @@ vsc="$(which code)"
 extensions=(
 bungcip.better-toml
 CoenraadS.bracket-pair-colorizer-2
+DavidAnson.vscode-markdownlint
 donjayamanne.githistory
 eamodio.gitlens
 Equinusocio.vsc-community-material-theme
@@ -17,6 +18,8 @@ equinusocio.vsc-material-theme-icons
 esbenp.prettier-vscode
 formulahendry.auto-rename-tag
 formulahendry.code-runner
+foxundermoon.shell-format
+Gruntfuggly.todo-tree
 humao.rest-client
 kamikillerto.vscode-colorize
 letmaik.git-tree-compare
@@ -24,19 +27,22 @@ mauve.terraform
 mjmcloug.vscode-elixir
 ms-azuretools.vscode-docker
 ms-python.python
+ms-vscode.Go
 ms-vsliveshare.vsliveshare
 oderwat.indent-rainbow
 PKief.material-icon-theme
 pranaygp.vscode-css-peek
 quicktype.quicktype
+redhat.vscode-yaml
+rogalmic.bash-debug
 rust-lang.rust
 serayuzgur.crates
 shardulm94.trailing-spaces
 SimonSiefke.svg-preview
 vscodevim.vim
+wayou.vscode-todo-highlight
 wholroyd.HCL
 yzhang.markdown-all-in-one
-redhat.vscode-yaml
 )
 
 for i in ${extensions[@]}; do
