a2433cb89fd2bacb46dd8476dc406674923fcf4d
Soren Martius <soeren.martius@gmail.com>
2020-06-22 10:57:02 +0200

    update vscode extensions

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -7,42 +7,52 @@
 vsc="$(which code)"
 
 extensions=(
-bungcip.better-toml
-CoenraadS.bracket-pair-colorizer-2
-DavidAnson.vscode-markdownlint
-donjayamanne.githistory
-eamodio.gitlens
-Equinusocio.vsc-community-material-theme
-Equinusocio.vsc-material-theme
-equinusocio.vsc-material-theme-icons
-esbenp.prettier-vscode
-formulahendry.auto-rename-tag
-formulahendry.code-runner
-foxundermoon.shell-format
-Gruntfuggly.todo-tree
-humao.rest-client
-kamikillerto.vscode-colorize
-letmaik.git-tree-compare
-mauve.terraform
-mjmcloug.vscode-elixir
-ms-azuretools.vscode-docker
-ms-python.python
-ms-vscode.Go
-ms-vsliveshare.vsliveshare
-oderwat.indent-rainbow
-PKief.material-icon-theme
-pranaygp.vscode-css-peek
-quicktype.quicktype
-redhat.vscode-yaml
-rogalmic.bash-debug
-rust-lang.rust
-serayuzgur.crates
-shardulm94.trailing-spaces
-SimonSiefke.svg-preview
-vscodevim.vim
-wayou.vscode-todo-highlight
-wholroyd.HCL
-yzhang.markdown-all-in-one
+    aaron-bond.better-comments
+    bungcip.better-toml
+    christian-kohler.path-intellisense
+    CoenraadS.bracket-pair-colorizer-2
+    DavidAnson.vscode-markdownlint
+    dbaeumer.vscode-eslint
+    donjayamanne.githistory
+    eamodio.gitlens
+    Equinusocio.vsc-community-material-theme
+    Equinusocio.vsc-material-theme
+    equinusocio.vsc-material-theme-icons
+    esbenp.prettier-vscode
+    formulahendry.auto-rename-tag
+    formulahendry.code-runner
+    foxundermoon.shell-format
+    GitHub.vscode-pull-request-github
+    golang.Go
+    Gruntfuggly.todo-tree
+    hashicorp.terraform
+    humao.rest-client
+    kamikillerto.vscode-colorize
+    letmaik.git-tree-compare
+    mjmcloug.vscode-elixir
+    ms-azuretools.vscode-azureterraform
+    ms-azuretools.vscode-docker
+    ms-python.python
+    ms-vscode.azure-account
+    ms-vsliveshare.vsliveshare
+    msjsdiag.debugger-for-chrome
+    oderwat.indent-rainbow
+    PKief.material-icon-theme
+    pranaygp.vscode-css-peek
+    quicktype.quicktype
+    redhat.vscode-yaml
+    ritwickdey.LiveServer
+    rogalmic.bash-debug
+    rust-lang.rust
+    serayuzgur.crates
+    shardulm94.trailing-spaces
+    SimonSiefke.svg-preview
+    streetsidesoftware.code-spell-checker
+    VisualStudioExptTeam.vscodeintellicode
+    vscodevim.vim
+    wayou.vscode-todo-highlight
+    wholroyd.HCL
+    yzhang.markdown-all-in-one
 )
 
 for i in ${extensions[@]}; do
